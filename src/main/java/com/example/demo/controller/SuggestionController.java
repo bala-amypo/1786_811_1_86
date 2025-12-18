c0494022a02977bec7f75a0112e49a81d62026cc
bala-amypo <bala@amypo.in>
2025-12-18 16:34:24 +0000

    User commit

--- src/main/java/com/example/demo/controller/SuggestionController.java
+++ src/main/java/com/example/demo/controller/SuggestionController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.Suggestion;
+import com.example.demo.entity.SuggestionEntity;
 import com.example.demo.service.SuggestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
