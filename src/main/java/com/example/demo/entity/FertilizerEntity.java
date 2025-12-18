ef282c5fa7a276a4b0705e4f2ad6d393bcfc656c
bala-amypo <bala@amypo.in>
2025-12-18 10:02:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/FertilizerEntity.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FertilizerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String npkRatio;
+
+    private String recommendedForCrops;
+}
