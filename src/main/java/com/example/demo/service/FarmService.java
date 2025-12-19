ca93f48aba422548ada5dd3d404d94d28dd2ed12
bala-amypo <bala@amypo.in>
2025-12-19 18:12:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/FarmService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.FarmEntity;
+
+public interface FarmService {
+
+    FarmEntity create(FarmEntity farm);
+
+    List<FarmEntity> getAll();
+
+    FarmEntity getById(Long id);
+
+    FarmEntity update(Long id, FarmEntity farm);
+
+    void delete(Long id);
+}
---
+++ src/main/java/com/example/demo/service/SuggestionService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.SuggestionEntity;
+
+public interface SuggestionService {
+
+    SuggestionEntity create(SuggestionEntity suggestion);
+
+    List<SuggestionEntity> getAll();
+
+    SuggestionEntity getById(Long id);
+
+    void delete(Long id);
+}
