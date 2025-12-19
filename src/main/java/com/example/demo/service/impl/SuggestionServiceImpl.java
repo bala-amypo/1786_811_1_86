b3d5fabbfd15fe39c50af1645c901c55df91c9c1
bala-amypo <bala@amypo.in>
2025-12-19 18:11:40 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/SuggestionServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.SuggestionEntity;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.SuggestionRepository;
+import com.example.demo.service.SuggestionService;
+
+@Service
+public class SuggestionServiceImpl implements SuggestionService {
+
+    private final SuggestionRepository repository;
+
+    public SuggestionServiceImpl(SuggestionRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public SuggestionEntity create(SuggestionEntity suggestion) {
+        return repository.save(suggestion);
+    }
+
+    @Override
+    public List<SuggestionEntity> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public SuggestionEntity getById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Suggestion not found with id: " + id));
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.delete(getById(id));
+    }
+}
