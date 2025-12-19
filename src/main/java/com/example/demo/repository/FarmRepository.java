7a8cadf533fde33b1b7b0abd92229a4b00f65498
bala-amypo <bala@amypo.in>
2025-12-19 06:47:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/FarmRepository.java
+++ src/main/java/com/example/demo/repository/FarmRepository.java
@@ -4,8 +4,11 @@ import com.example.demo.entity.FarmEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FarmRepository extends JpaRepository<FarmEntity, Long> {
 
-    List<Farm> findByOwnerId(Long ownerId);
+    List<FarmEntity> findByOwnerId(Long ownerId);
+
+    Optional<FarmEntity> findByIdAndOwnerId(Long farmId, Long ownerId);
 }
