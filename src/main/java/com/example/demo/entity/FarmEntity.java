de4216476787e213567e648afe6f7588db435c39
bala-amypo <bala@amypo.in>
2025-12-19 06:32:54 +0000

    User commit

--- src/main/java/com/example/demo/entity/FarmEntity.java
+++ src/main/java/com/example/demo/entity/FarmEntity.java
@@ -1,30 +1,38 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 @Entity
+@Table(name = "farms")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class FarmEntity{
+public class FarmEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
     private UserEntity owner;
 
+    @NotBlank
+    @Column(length = 100)
     private String name;
 
+    @NotNull
+    @Column(name = "soil_ph")
     private Double soilPH;
 
+    @NotNull
+    @Column(name = "water_level")
     private Double waterLevel;
 
+    @NotBlank
     private String season;
 }
