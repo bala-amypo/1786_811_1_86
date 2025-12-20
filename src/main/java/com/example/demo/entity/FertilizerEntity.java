2e54abbb5bc81f62354e5d35ad38c1d858225f11
bala-amypo <bala@amypo.in>
2025-12-20 08:41:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/FertilizerEntity.java
+++ src/main/java/com/example/demo/entity/FertilizerEntity.java
@@ -26,14 +26,6 @@ public class FertilizerEntity {
 
     public FertilizerEntity() {}
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
