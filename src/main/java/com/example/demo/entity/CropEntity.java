dad33bd0810438488818ee3b4662228d480721c0
bala-amypo <bala@amypo.in>
2025-12-20 08:41:07 +0000

    User commit

--- src/main/java/com/example/demo/entity/CropEntity.java
+++ src/main/java/com/example/demo/entity/CropEntity.java
@@ -32,13 +32,6 @@ public class CropEntity {
 
     public CropEntity() {}
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
