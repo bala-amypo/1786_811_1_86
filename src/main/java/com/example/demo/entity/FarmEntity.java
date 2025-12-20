0f932b634052a69023f2f55d9614fa268e85cb73
bala-amypo <bala@amypo.in>
2025-12-20 08:50:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/FarmEntity.java
+++ src/main/java/com/example/demo/entity/FarmEntity.java
@@ -33,14 +33,6 @@ public class FarmEntity {
 
     public FarmEntity() {}
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
