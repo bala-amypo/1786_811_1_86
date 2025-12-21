2cd93e68b277e5e2cc4d26a867561849f3e224c6
bala-amypo <bala@amypo.in>
2025-12-21 13:38:24 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CatalogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CatalogServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.CropEntity;
 import com.example.demo.entity.FertilizerEntity;
-import com.example.demo.exception.AlreadyExistsException;
 import com.example.demo.repository.CropRepository;
 import com.example.demo.repository.FertilizerRepository;
 import com.example.demo.service.CatalogService;
@@ -25,11 +24,6 @@ public class CatalogServiceImpl implements CatalogService {
 
     @Override
     public CropEntity addCrop(CropEntity crop) {
-
-        if (cropRepository.findByName(crop.getName()).isPresent()) {
-            throw new AlreadyExistsException("Crop already exists");
-        }
-
         return cropRepository.save(crop);
     }
 
@@ -40,11 +34,6 @@ public class CatalogServiceImpl implements CatalogService {
 
     @Override
     public FertilizerEntity addFertilizer(FertilizerEntity fertilizer) {
-
-        if (fertilizerRepository.findByName(fertilizer.getName()).isPresent()) {
-            throw new AlreadyExistsException("Fertilizer already exists");
-        }
-
         return fertilizerRepository.save(fertilizer);
     }
 
