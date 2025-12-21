58c37246f4f229a1bf5f968339aba77748045f8d
bala-amypo <bala@amypo.in>
2025-12-21 08:56:04 +0000

    User commit

--- src/main/java/com/example/demo/repository/FertilizerRepository.java
+++ src/main/java/com/example/demo/repository/FertilizerRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +9,7 @@ import com.example.demo.entity.FertilizerEntity;
 
 public interface FertilizerRepository extends JpaRepository<FertilizerEntity, Long> {
 
+    Optional<FertilizerEntity> findByName(String name);
+
     List<FertilizerEntity> findByRecommendedForCropsContaining(String cropName);
 }
-
