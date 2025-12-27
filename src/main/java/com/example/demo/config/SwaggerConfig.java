3d308d2fed95a7126f44c756acc89d13123dc2b2
bala-amypo <bala@amypo.in>
2025-12-27 06:12:18 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -5,13 +5,15 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import java.util.List;
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI api() {
         return new OpenAPI()
-                .server
+                .servers(List.of(
+                        new Server().url("https://9384.pro604cr.amypo.ai/")
+                ));
                 .info(new Info().title("Crop & Fertilizer Suggestion API").version("1.0"))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
